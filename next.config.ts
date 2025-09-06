22a1f33912ab7e309041c965622922205b0d326c
Tanvin-Ahmed <tanvinahmedtouhid91@gmail.com>
2025-09-06 12:53:48 +0600

    update details page logic and next.config fie

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["https://event-management-task-delta.vercel.app"],
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,36 +1,18 @@
-import { Event } from "@/types";
-import axios from "axios";
-import { notFound } from "next/navigation";
 import EventDetailsClient from "@/components/EventDetailsClient";
+import { getEventById } from "@/data/eventsStore";
+
+import { notFound } from "next/navigation";
 
 interface EventDetailsPageProps {
   params: Promise<{ id: string }>;
-}
-
-async function fetchEventById(id: string): Promise<Event | null> {
-  try {
-    const baseUrl =
-      process.env.NODE_ENV === "production"
-        ? `${process.env.VERCEL_URL}`
-        : "http://localhost:3000";
-
-    const response = await axios.get(`${baseUrl}/api/events/${id}`);
-
-    if (response.data.success) {
-      return response.data.data;
-    }
-
-    return null;
-  } catch {
-    return null;
-  }
 }
 
 export default async function EventDetailsPage({
   params,
 }: EventDetailsPageProps) {
   const { id } = await params;
-  const event = await fetchEventById(id);
+
+  const event = getEventById(id);
 
   if (!event) {
     notFound();
